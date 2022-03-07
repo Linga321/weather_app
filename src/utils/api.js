7f1a7b099fe1ea6813c863ff58491073c7970151
Gajalakshan Chandrasegaran <56063292+Linga321@users.noreply.github.com>
2022-03-07 13:09:20 +0200

    For requested an secure resource

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const API_URL_LOCATION = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/';
-const API_URL_KEY = 'http://dataservice.accuweather.com/locations/v1/cities/search?';
+const API_URL_LOCATION = 'https://dataservice.accuweather.com/forecasts/v1/daily/1day/';
+const API_URL_KEY = 'https://dataservice.accuweather.com/locations/v1/cities/search?';
 
 export { API_URL_LOCATION , API_URL_KEY};
